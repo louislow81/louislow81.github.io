6b710ce6be43d7db08a275bd9af87ed4bffbfb05
Loouis Low <loouis@gmail.com>
2019-04-13 13:34:32 +0800

    clean up

--- assets/js/golemCacheMecha_settings.js
+++ assets/js/golemCacheMecha_settings.js
@@ -9,17 +9,27 @@ var golemCacheSettings = function () {
 // init
 golemCacheSettings();
 
-// asynchronous scripts manager
+// asynchronous stashing manager,
+// items that are stored into persistent storage
+// for supported assets (js, css)
+
+// (proto) extended support for other scripts (xml, yaml, json)
+// (proto) extended support for fonts (ttf, eot)
+
+// (css)/(font) would be slightly different behaviour, should store in
+// persistent storage at first before calling with <link> tag
+// without doing injecting like (js).
+
 golemCacheMecha.add({
   url: 'https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600',
-  key: 'google-fonts'
+  key: 'item-google-fonts'
 }, {
   url: 'https://use.fontawesome.com/releases/v5.1.1/css/all.css',
-  key: 'fontawesome'
+  key: 'item-fontawesome'
 }, {
   url: 'assets/css/ui.min.css',
-  key: 'ui.min.css'
+  key: 'item-main-css'
 }, {
   url: 'assets/js/app.min.js',
-  key: 'app.min.js'
+  key: 'item-main-js'
 });
