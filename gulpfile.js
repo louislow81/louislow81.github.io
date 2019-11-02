83d36131967f302c2d28f70766a05af932d089b5
Loouis Low <loouis@gmail.com>
2019-11-02 21:27:38 +0800

    refactoring `gulpfile`

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const concatCss = require('gulp-concat-css')
-const uglifycss = require('gulp-uglifycss')
+const uglifyCss = require('gulp-uglifycss')
 const sassGlob = require('gulp-sass-glob')
 const browserSync = require('browser-sync').create()
-const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
-const cssvariables = require('postcss-css-variables')
+const postCss = require('gulp-postcss')
+const autoPrefixer = require('autoprefixer')
+const cssVariables = require('postcss-css-variables')
 const calc = require('postcss-calc')
 const concat = require('gulp-concat')
 const rename = require('gulp-rename')
@@ -15,23 +15,38 @@ const htmlmin = require('gulp-htmlmin')
 const imagemin = require('gulp-imagemin')
 const pngquant = require('imagemin-pngquant')
 const mozjpeg = require('imagemin-mozjpeg')
+const gutil = require('gulp-util')
+const ftp = require('vinyl-ftp')
 
 
-// html file paths
-const htmlFilesPath = 'src/views/**/*.html'
-const htmlViewsPath = 'dist' // production-ready files
+// html
+const srcHtmlPath = 'src/views/**/*.html'
 
-// js file paths
-const utilJsPath = 'src/assets/js' // util.js path - you may need to update this if including the framework as external node module
-const componentsJsPath = 'src/assets/js/components/*.js'
-const scriptsJsPath = 'dist/assets/js' // folder for final scripts.js/scripts.min.js files
+// js
+const srcUtilJsPath = 'src/assets/js'
+const srcComponentsJsPath = 'src/assets/js/components/*.js'
+const distJsPath = 'dist/assets/js'
 
-// scss/css file paths
-const scssFilesPath = 'src/assets/scss/style.scss'
-const cssPath = 'dist/assets/css' // folder for final style.css/style-custom-prop-fallback.css files
+// scss/css
+const srcScssPath = 'src/assets/scss/style.scss'
+const distCssPath = 'dist/assets/css'
 
-// image file paths
-const imagePath = 'dist/assets/image' // folder for final optimized images
+// image
+const srcImageRecursivePath = 'src/assets/image/**/*'
+const distImagePath = 'dist/assets/image'
+
+// production
+const distProdPath = 'dist'
+const distProdRecursivePath = 'dist/**/*'
+
+// ftp
+const ftpDestPath = '/public_html/www' // set yours
+
+// watch
+const watchSrcHtmlPath = 'src/views/**/*.html'
+const watchSrcScssPath = 'src/assets/scss/**/*.scss'
+const watchSrcScriptsPath = 'src/assets/js/**/*.js'
+const watchSrcImagePath = 'src/assets/image/**/*'
 
 
 // reload web browser
@@ -41,98 +56,154 @@ function reload(done) {
 }
 
 
+// ...for ftp
+gulp.task('deploy',
+  function() {
+
+    const conn = ftp.create({
+      host: 'localhost',
+      port: 21,
+      user: 'anonymous',
+      password: 'anonymous',
+      parallel: 1,
+      maxConnections: 1,
+      secure: false
+    })
+
+    const globs = [
+      distProdRecursivePath
+    ]
+
+    return gulp.src(globs, {
+        base: distProdPath + '/',
+        buffer: false
+      })
+      .pipe(conn.newer(ftpDestPath)) // only upload newer files
+      .pipe(conn.dest(ftpDestPath))
+  }
+)
+
+
 // serve http
-gulp.task('browserSync', gulp.series(function(done) {
-  browserSync.init({
-    server: {
-      baseDir: 'dist'
-    },
-    notify: false
+gulp.task('browserSync',
+  gulp.series(function(done) {
+    browserSync.init({
+      server: {
+        baseDir: distProdPath
+      },
+      notify: false
+    })
+    done()
   })
-  done()
-}))
+)
 
 
 // ...for html
-gulp.task('html', function() {
-  return gulp.src(htmlFilesPath)
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      removeComments: true,
-      jsmin: true, // inline js
-      cssmin: true // inline css
-    }))
-    .pipe(gulp.dest(htmlViewsPath))
-})
+gulp.task('html',
+  function() {
+    return gulp.src(srcHtmlPath)
+      .pipe(htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        jsmin: true, // inline js
+        cssmin: true // inline css
+      }))
+      .pipe(gulp.dest(distProdPath))
+  }
+)
 
 
 // ...for scss
-gulp.task('sass', function() {
-  return gulp.src(scssFilesPath)
-    .pipe(sassGlob())
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest(cssPath))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-    .pipe(rename('style-fallback.css'))
-    .pipe(postcss([cssvariables(), calc()]))
-    .pipe(gulp.dest(cssPath))
-})
+gulp.task('sass',
+  function() {
+    return gulp.src(srcScssPath)
+      .pipe(sassGlob())
+      .pipe(sass({ outputStyle: 'compressed' })
+        .on('error', sass.logError))
+      .pipe(postCss([autoPrefixer()]))
+      .pipe(gulp.dest(distCssPath))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+      .pipe(rename('style-fallback.css'))
+      .pipe(postCss([cssVariables(), calc()]))
+      .pipe(gulp.dest(distCssPath))
+  }
+)
 
 
 // ...for js
-gulp.task('scripts', function() {
-  return gulp.src([
-      utilJsPath + '/util.js',
-      componentsJsPath
-    ])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest(scriptsJsPath))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-    .pipe(rename('scripts.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(scriptsJsPath))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-})
+gulp.task('scripts',
+  function() {
+    return gulp.src([
+        srcUtilJsPath + '/util.js',
+        srcComponentsJsPath
+      ])
+      .pipe(concat('scripts.js'))
+      .pipe(gulp.dest(distJsPath))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+      .pipe(rename('scripts.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(distJsPath))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+  }
+)
 
 
 // ...for proprietary library
-gulp.task('proprietary', function() {
-  return gulp.src([
-      utilJsPath + '/krunch+compiler.min.js'
-    ])
-    .pipe(gulp.dest(scriptsJsPath))
-})
+gulp.task('proprietary',
+  function() {
+    return gulp.src([
+        srcUtilJsPath + '/krunch+compiler.min.js'
+      ])
+      .pipe(gulp.dest(distJsPath))
+  }
+)
 
 
 // ...for image
-gulp.task('image', function() {
-  return gulp.src('src/assets/image/**/*')
-    .pipe(imagemin([
-      pngquant({ quality: [1, 1] }), // png
-      mozjpeg({ quality: 100 }), // jpg
-    ]))
-    .pipe(gulp.dest(imagePath))
-})
+gulp.task('image',
+  function() {
+    return gulp.src(srcImageRecursivePath)
+      .pipe(imagemin([
+        pngquant({ quality: [1, 1] }), // png
+        mozjpeg({ quality: 100 }), // jpg
+      ]))
+      .pipe(gulp.dest(distImagePath))
+  }
+)
 
 
-// watch those assets changes...
-gulp.task('watch', gulp.series([
-  'browserSync', 
-  'html', 
-  'sass', 
-  'scripts', 
-  'image', 
-  'proprietary'
+// watch assets changes...
+gulp.task('watch',
+  gulp.series([
+
+    'browserSync',
+    'html',
+    'sass',
+    'scripts',
+    'image',
+    'proprietary',
+    'deploy'
+
   ], function() {
-  gulp.watch('src/views/**/*.html', gulp.series(['html', reload]))
-  gulp.watch('src/assets/scss/**/*.scss', gulp.series(['sass', reload]))
-  gulp.watch('src/assets/js/**/*.js', gulp.series(['scripts']))
-  gulp.watch('src/assets/image/**/*', gulp.series('image', reload))
-}))
+
+    gulp.watch(watchSrcHtmlPath,
+      gulp.series(['html', reload]))
+
+    gulp.watch(watchSrcScssPath,
+      gulp.series(['sass', reload]))
+
+    gulp.watch(watchSrcScriptsPath,
+      gulp.series(['scripts']))
+
+    gulp.watch(watchSrcImagePath,
+      gulp.series('image', reload))
+
+  })
+
+)
