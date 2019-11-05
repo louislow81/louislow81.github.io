8b5c78fbc2e8e35c26c6db4250ec656e16310066
Loouis Low <loouis@gmail.com>
2019-11-05 08:23:42 +0800

    added Twitter updater

---
+++ src/assets/js/components/twitter-updater.js
@@ -0,0 +1,17 @@
+window.twttr = (function(d, s, id) {
+  var t, js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+  return window.twttr || (t = { _e: [], ready: function(f) { t._e.push(f) } });
+}(document, "script", "twitter-wjs"));
+
+twttr.ready(function(twttr) {
+  twttr.widgets.load();
+  setInterval(function() {
+    twttr.widgets.load();
+    // console.log("update twitter timeline");
+  }, 1000);
+});
