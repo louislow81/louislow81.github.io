dc0fbdb9e27e5696b79abcea18657f7a354f154b
Loouis Low <loouis@gmail.com>
2019-11-05 08:23:42 +0800

    added Twitter updater

--- src/assets/js/components/twitter-overrides.js
+++ src/assets/js/components/twitter-overrides.js
@@ -1,31 +1,31 @@
-$('.twitter-feed').delegate('#twitter-widget-0', 'DOMSubtreeModified propertychange', function() {
-  customizeTweetMedia();
-});
+// $('.twitter-feed').delegate('#twitter-widget-0', 'DOMSubtreeModified propertychange', function() {
+//   customizeTweetMedia();
+// });
 
-var customizeTweetMedia = function() {
+// var customizeTweetMedia = function() {
 
-  // CSS Overrides
-  $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-media').css('display', 'block');
+//   // CSS Overrides
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-media').css('display', 'block');
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('img.Avatar').css('display', 'none');
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('img.Avatar').css('display', 'none');
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-avatar.Identity-avatar').remove();
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-avatar.Identity-avatar').remove();
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-screenName').css('font-size', '16px');
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-screenName').css('font-size', '16px');
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-screenName').css('font-family', 'Raleway');
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-screenName').css('font-family', 'Raleway');
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('font-family', 'Poppins');
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('font-family', 'Poppins');
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('font-size', '16px').css('color', '#ffffff');
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('font-size', '16px').css('color', '#ffffff');
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('line-height', '1.6');
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('line-height', '1.6');
 
-  $('.twitter-feed').find('.twitter-timeline').contents().find('ul.timeline-tweet-actions').css('display', 'none');
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('ul.timeline-tweet-actions').css('display', 'none');
 
-  // Call the function on dynamic updates in addition to page load
-  $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList').bind('DOMSubtreeModified propertychange', function() {
-    customizeTweetMedia(this);
-  });
+//   // Call the function on dynamic updates in addition to page load
+//   $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList').bind('DOMSubtreeModified propertychange', function() {
+//     customizeTweetMedia(this);
+//   });
 
-}
+// }
