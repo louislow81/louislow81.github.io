d5662e0e0ea3b856bd546fd2efc6e9643881984c
Loouis Low <loouis@gmail.com>
2020-01-15 01:44:12 +0800

    added PWA apps

---
+++ apps/kraft/assets/js/init.js
@@ -0,0 +1,13 @@
+//
+// @@script: init.js
+// @@description: kraft bootstrapper
+// @@version:
+// @@author: Loouis Low
+// @@copyright: dogsbark Inc
+//
+
+const script = document.createElement("script")
+
+script.src = "./assets/js/app.min.js"
+
+document.body.appendChild(script)
