40e645e82b18a0d42e6e7c7eb724c8a728429cbf
Loouis Low <loouis@gmail.com>
2020-01-16 17:44:24 +0800

    corrected hour value for `morning`, `evening`, `night`

--- src/assets/js/components/_greeting_with_time.js
+++ src/assets/js/components/_greeting_with_time.js
@@ -5,13 +5,13 @@ greeting.withTime = function() {
   const time = new Date().getHours();
   const msg = " Are you come here to hire me?";
 
-  if (time < 10) { // morning
+  if (time < 12) { // morning
 
     const yelling = '<strong>Good morning!</strong>' + msg;
     snicker.onLoad(yelling, 15000);
 
   }
-  else if (time < 18) { // evening
+  else if (time < 20) { // evening
 
     const yelling = '<strong>Good evening!</strong>' + msg;
     snicker.onLoad(yelling, 15000);
