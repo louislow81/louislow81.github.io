8491f4d40d091d07fc08097429ad57e2cf8a89cc
Loouis Low <loouis@gmail.com>
2020-01-16 17:32:19 +0800

    updated from branch `2.x.x`

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -255,6 +255,34 @@ snicker.onLoad = function(data, duration) {
   snickerUI(data, duration);
 };
 
+function message() {};
+
+message.timezone = function() {
+
+  const time = new Date().getHours();
+  const msg = " Are you come here to hire me?";
+
+  if (time < 10) { // morning
+
+    const yelling = '<strong>Good morning!</strong>' + msg;
+    snicker.onLoad(yelling, 15000);
+
+  }
+  else if (time < 18) { // evening
+
+    const yelling = '<strong>Good evening!</strong>' + msg;
+    snicker.onLoad(yelling, 15000);
+
+  }
+  else { // night
+
+    const yelling = '<strong>Good night!</strong>' + msg;
+    snicker.onLoad(yelling, 15000);
+
+  }
+
+};
+
 /*
   Get Total Posts from JSON data
   @param {id}
