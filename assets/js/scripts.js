576293feae27f7d6cd7589b9c63ec7f5278fff2c
Loouis Low <loouis@gmail.com>
2020-01-16 17:34:01 +0800

    display messages on different time in hour

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -148,6 +148,34 @@ krunch.isOnline = function() {
 };
 
 
+function greeting() {};
+
+greeting.withTime = function() {
+
+  const time = new Date().getHours();
+  const msg = " Are you come here to hire me?";
+
+  if (time < 10) { // morning
+
+    const yelling = '<strong>Good morning!</strong>' + msg;
+    snicker.onLoad(yelling, 15000);
+
+  }
+  else if (time < 18) { // evening
+
+    const yelling = '<strong>Good evening!</strong>' + msg;
+    snicker.onLoad(yelling, 15000);
+
+  }
+  else { // night
+
+    const yelling = '<strong>Good night!</strong>' + msg;
+    snicker.onLoad(yelling, 15000);
+
+  }
+
+};
+
 const reader = {
 
   "defaults": {
@@ -253,34 +281,6 @@ snicker.onClick = function(id, data, duration) {
 */
 snicker.onLoad = function(data, duration) {
   snickerUI(data, duration);
-};
-
-function message() {};
-
-message.timezone = function() {
-
-  const time = new Date().getHours();
-  const msg = " Are you come here to hire me?";
-
-  if (time < 10) { // morning
-
-    const yelling = '<strong>Good morning!</strong>' + msg;
-    snicker.onLoad(yelling, 15000);
-
-  }
-  else if (time < 18) { // evening
-
-    const yelling = '<strong>Good evening!</strong>' + msg;
-    snicker.onLoad(yelling, 15000);
-
-  }
-  else { // night
-
-    const yelling = '<strong>Good night!</strong>' + msg;
-    snicker.onLoad(yelling, 15000);
-
-  }
-
 };
 
 /*
