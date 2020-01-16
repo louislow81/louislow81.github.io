f011ff1b5799427b60095e464dc7cd5fb36dc1f2
Loouis Low <loouis@gmail.com>
2020-01-16 17:47:51 +0800

    corrected mis-typos

--- src/assets/js/components/_greeting_with_time.js
+++ src/assets/js/components/_greeting_with_time.js
@@ -3,7 +3,7 @@ function greeting() {};
 greeting.withTime = function() {
 
   const time = new Date().getHours();
-  const msg = " Are you come here to hire me?";
+  const msg = " Are you coming here to hire me?";
 
   if (time < 12) { // morning
 
