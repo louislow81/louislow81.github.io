86af2b47fd9ecfe6efeaa175b5797122c41abb68
Loouis Low <loouis@gmail.com>
2020-01-17 18:07:16 +0800

    added experimental funcs/modules `adaptive image loader`, `browser network properties`

--- src/assets/js/krunch+utility.js
+++ src/assets/js/krunch+utility.js
@@ -8,9 +8,9 @@ function krunch() {};
   @enable with `console.log(msg, req);`
   @disable with `console.log();`
 */
-const log = function(msg, req){
-  // console.log('krugurt:', msg, req);
-  console.log();
+const log = function(msg, req) {
+  console.log('krugurt:', msg, req);
+  // console.log();
 };
 
 
@@ -22,8 +22,10 @@ const log = function(msg, req){
   @param {htmlstring}
 */
 function injectDOM(htmlStr) {
-  const frag = document.createDocumentFragment(),
-    temp = document.createElement('y');
+
+  const frag = document.createDocumentFragment();
+  const temp = document.createElement('y');
+
   temp.innerHTML = htmlStr;
   while (temp.firstChild) {
     frag.appendChild(temp.firstChild);
@@ -43,17 +45,20 @@ krunch.probeConnection = function() {
 
   try {
     window.addEventListener('load', function() {
+
       function checkStatus() {
         // display status
         window.document.getElementById('ba194bb5a0b6e42d520d17a3b75f5962')
           .className = navigator.onLine ? 'is-online' : 'is-offline';
         log('(CONN) is ' + window.document.getElementById('ba194bb5a0b6e42d520d17a3b75f5962').className);
       }
+
       setInterval(function() {
         // check connection
         window.addEventListener('online', checkStatus);
         window.addEventListener('offline', checkStatus);
       }, 1000)
+
     });
   }
   catch (error) {
@@ -120,3 +125,133 @@ krunch.isOnline = function() {
   });
 };
 
+
+/*
+  Localization processor
+  @methofOf locale
+  @param {id} html element id
+  @param {data} specify payload from json e.g. 'common:button.save'
+*/
+function localeProcessor(id, data) {
+
+  const getId = id;
+  let getData = data;
+
+  locale
+    .use(XHRBackend)
+    .use(BrowserLanguageDetector)
+    .init({ // init processor
+      fallbackLang: 'en',
+      debug: true,
+      namespace: ['special', 'common'],
+      defaultNS: 'special',
+      backend: {
+        loadPath: 'assets/locale/{{lng}}/{{namespace}}.json', // !! default
+        crossDomain: true
+      }
+    }, function(err, t) {
+      updatePayload(getId, getData); // init set content
+    });
+
+  function updatePayload(getId, getData) {
+
+    const getElementId = document.getElementById(getId);
+    let getLocaleValue = locale.t(getData);
+
+    getElementId.innerHTML = getLocaleValue;
+  };
+
+  locale.on('languageChanged', function() {
+    updatePayload(getId, getData);
+  });
+
+};
+
+
+/*
+  Localization Worker
+  @param {array}
+*/
+function localeGenerator(array) {
+  for (let i = 0; i < array.length; i++) {
+    let obj = array[i]
+    localeProcessor(obj.id, obj.data);
+  }
+};
+
+
+/*
+  Get ids and initialize localization
+*/
+krunch.lang = function() {
+
+  const file = 'assets/locale/id.json';
+
+  fetch(file).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    localeGenerator(data);
+    log('(LANG) (id)', data);
+  }).catch(function(err) {
+    log('(LANG) (ERR)', err);
+  });
+};
+
+
+/*
+  Trigger for changing locales
+  @param {lang} as in e.g. 'en', 'de'
+*/
+krunch.langTrigger = function(lang) {
+  locale.changeLanguage(lang);
+};
+
+
+/*
+  Adaptive Image Loader
+  @param {null}
+  @usage,
+    <img class="adaptive"
+         src="assets/image/low/image.jpg"
+         data-src="assets/image/high/image.jpg">
+*/
+krunch.adaptiveImageLoader = function() {
+
+  const images = document.getElementsByClassName('adaptive')
+
+  Array.from(images).map(imageElement => {
+
+    const adaptive = new Image() // start loading image
+    adaptive.src = imageElement.dataset.src
+
+    // once image is loaded replace the src of the HTML element
+    adaptive.onload = function() {
+      imageElement.classList.remove('adaptive')
+      if ('IMG' === imageElement.nodeName) imageElement.src = adaptive.src
+      else imageElement.style.backgroundImage = `url(${adaptive.src})`
+    }
+
+  })
+};
+
+
+/*
+  Display browser network properties
+  @param {null}
+ */
+krunch.networkSpeed = function() {
+  // network type that browser uses
+  log('(Network Type) ' + navigator.connection.type);
+  // effective bandwidth estimate
+  log('(Downlink) ' + navigator.connection.downlink + ' MBytes/s', '');
+  // effective round-trip time estimate
+  log('(Round-Trip Time) ' + navigator.connection.rtt + ' miliseconds', '');
+  // upper bound on the downlink speed of the first network hop
+  log('(Downlink Max) ' + navigator.connection.downlinkMax + ' MBytes/s', '');
+  // effective connection type determined using a combination of recently
+  // observed rtt and downlink values: ' +
+  ('(Effective Type) ' + navigator.connection.effectiveType, '');
+  // true if the user has requested a reduced data usage mode from the
+  // user agent.
+  log('(DataSaver Mode) ' + navigator.connection.saveData, '');
+};
