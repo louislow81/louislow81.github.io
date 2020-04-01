1896971e190d92da56bffce14f1c15944abf62a3
Loouis Low <loouis@gmail.com>
2020-04-02 01:26:42 +0800

    disable `check connection` and `show network properties` from Krugurt.js

--- src/views/app.js
+++ src/views/app.js
@@ -4,8 +4,8 @@
 */
 (async function main() {
 
-  krunch.probeConnection() // check connection
-  krunch.networkSpeed() // show network properties
+  // krunch.probeConnection() // check connection
+  // krunch.networkSpeed() // show network properties
   progressbar.pageRead() // show reading progress
 
   await krunch.compile() // init compiler
