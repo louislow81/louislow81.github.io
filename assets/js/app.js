ffd12029257247943696d7ba38a5a76aa1d8a1f6
Loouis Low <loouis@gmail.com>
2020-04-13 07:49:08 +0800

    update from branch `2.x.x`

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,8 +20,8 @@ function krunch() {}
   @disable with `console.log();`
 */
 const log = function(msg, req) {
-  // console.log("krugurt:", msg, req);
-  console.log();
+  console.log("krugurt:", msg, req);
+  // console.log();
 };
 
 
